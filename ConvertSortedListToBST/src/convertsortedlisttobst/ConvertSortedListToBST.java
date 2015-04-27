e62274fbf261af76be79394b427a7adb0e1ba228
Sandy <qm0920@hotmail.com>
2015-04-27 16:21:26 -0400

    bst update

--- ConvertSortedListToBST/src/convertsortedlisttobst/ConvertSortedListToBST.java
+++ ConvertSortedListToBST/src/convertsortedlisttobst/ConvertSortedListToBST.java
@@ -1,8 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
-Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+ Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
  */
-
 package convertsortedlisttobst;
 
 /**
@@ -17,5 +16,62 @@ public class ConvertSortedListToBST {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    private ListNode node;
+
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        int size = 0;
+        ListNode runner = head;
+        node = head;
+
+        while (runner != null) {
+            runner = runner.next;
+            size++;
+        }
+
+        return inorderHelper(0, size - 1);
+    }
+
+    public TreeNode inorderHelper(int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = start + (end - start) / 2;
+        TreeNode left = inorderHelper(start, mid - 1);
+
+        TreeNode treenode = new TreeNode(node.val);
+        treenode.left = left;
+        node = node.next;
+
+        TreeNode right = inorderHelper(mid + 1, end);
+        treenode.right = right;
+
+        return treenode;
+    }
+}
+
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
 }
